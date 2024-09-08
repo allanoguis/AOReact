4665ed6ab13fc741532af0a07fff43cd813d089e
Allan Oguis <allanoguis@gmail.com>
2024-09-09 03:13:38 +0800

    test
    ui elements and alert

--- allanoguis/src/App.js
+++ allanoguis/src/App.js
@@ -1,15 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+function MyButton() {
+  function handleClick() {
+    alert("Titi! 8===D");
+  }
+  return <button onClick={handleClick}> Click ME! </button>;
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <h1>Hello World</h1>
+        <MyButton />
+        <h2>testsetsetsetsttest</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
